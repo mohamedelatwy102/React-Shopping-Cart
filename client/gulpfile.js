51d7ef186667faf31d96fce18cfdf11f42464ef8
mohamedelatwy <mohamedelatwy@std.mans.edu.eg>
2022-03-31 15:57:14 +0200

    Header-cmmp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,5 +2,5 @@ const gulp = require('gulp')
 const gulpSass = require('gulp-sass')
 const sass = gulpSass(require('sass'))
 gulp.task ('sass' , function () {
-    gulp.src('src/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+    gulp.src('src/Components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
 })
---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../css/Header/Header.css'
+function Header() {
+    return ( 
+        <React.Fragment>
+            <header>Shopping Cart</header>
+        </React.Fragment>
+     );
+}
+
+export default Header;
